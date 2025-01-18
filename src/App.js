1e5ba47827f6ecb9f0f2772115f4fae0a0b32dcd
Mashrufa Orchi <165407256+mashrufaorc@users.noreply.github.com>
2025-01-18 18:10:58 -0500

    final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact element={ <Home />}></Route>
+          <Route path='/services' exact element={ <Services />}></Route>
+          <Route path='/products' exact element={ <Products />}></Route>
+          <Route path='/sign-up' exact element={ <SignUp />}></Route>
         </Routes>
       </Router>
     </>
